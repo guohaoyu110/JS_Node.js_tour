647c25992e3216b165a26ad1fb9c84572bee2e5e
guohaoyu110 <guohaoyu110@gmail.com>
2020-04-02 12:20:02 -0700

    callback function implement

--- node-basics/index.js
+++ node-basics/index.js
@@ -7,16 +7,28 @@ var rect = require('./rectangle');
 function solveRect (l,b){
     console.log ("Solving for rectangle with l= " + l + ' and b = ' + b );
 
-    if (l <= 0 || b <= 0){
-        console.log('Rectangle dimensions should be greater than 0');
-    }
-    else {
-        console.log('The area of the rectangle is '+ rect.area(l,b));
-        console.log('The perimeter of the rectangle is '+ rect.perimeter(l,b) );
+    // if (l <= 0 || b <= 0){
+    //     console.log('Rectangle dimensions should be greater than 0');
+    // }
+    // else {
+    //     console.log('The area of the rectangle is '+ rect.area(l,b));
+    //     console.log('The perimeter of the rectangle is '+ rect.perimeter(l,b) );
         
-    }
+    // }
+    rect(l, b, (err,rectangle) => {
+        if (err){
+            console.log('Error: ', err.message );
+        }
+        else {
+            console.log('The area of the rectangle of dimensions l = '+ l + " and b = "+ b + " is " + rectangle.area());
+            console.log('The parameter of the rectangle is ' + rectangle.perimeter());
+            
+        }
+    });
+    console.log('This statement if after the call to rectangle');
+    
 }
-
+// this is a 2 second delay that we introduced using the settimeout
 solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
--- node-basics/rectangle.js
+++ node-basics/rectangle.js
@@ -1,3 +1,23 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+// exports.perimeter = (x,y) => (2*(x+y));
+// exports.area = (x,y) => (x*y);
 
+// modify it to take the help of callbacks
+
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0){
+        setTimeout(() => callback(new Error("Rectangle dimensions should be  x= " + x + ' and y = ' + y), null),
+         2000); // the second parameter is time period
+        // use this function to delay before the callback function is called.
+        // whatever is begin done in this rectangle is going to take some time.
+        // this is asynchronous processing
+        console.log('Rectangle dimensions should be greater than 0');
+    }
+    else {
+        setTimeout(() => callback(null, {
+            perimeter: () => (2*(x+y)),
+            area: () => (x*y) // 前面不需要写x和y
+        }),
+         2000);
+         // a two second interval before the value will be passed back by this function 
+    }
+}
